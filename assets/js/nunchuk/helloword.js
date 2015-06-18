8cd6ad5c95b2add36904f636c927baa37146a2b6
jwogrady <john@status26.com>
2015-06-18 01:00:18 -0500

    improved tasked automation

---
+++ assets/js/nunchuk/helloword.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement('h1', null, 'Hello, Woswerwrwedsrld!fsdfafd'),
+  document.getElementById('example')
+);
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp'),
+    react = require('gulp-react'),
     concat = require('gulp-concat'),
     less = require('gulp-less'),
+    del = require('del'),
+    vinylPaths = require('vinyl-paths'),
     livereload = require('gulp-livereload'),
     LessPluginAutoPrefix = require('less-plugin-autoprefix'),
     autoprefix = new LessPluginAutoPrefix({
         browsers: ['last 2 version']
     });
 
-// Styles
-gulp.task('styles', function() {
+
+gulp.task('clean', function(cb) {
+    del(['.tmp'], cb);
+});
+
+gulp.task('compjsx', function() {
+    return gulp.src('assets/jsx/*.jsx')
+        .pipe(react())
+        .pipe(gulp.dest('assets/js/nunchuk'))
+
+});
+
+gulp.task('compless', function() {
     return gulp.src('assets/less/*.less')
         .pipe(concat('nunchuk.css'))
         .pipe(less({
@@ -17,40 +31,40 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('.tmp/public/css/'))
 });
 
-// Scripts
-// concatenate javascript files
-gulp.task('scripts', function() {
-    return gulp.src('assets/js/**/*.js')
-        .pipe(concat('bootstrap.js'))
+gulp.task('concatjs', ['compjsx'], function() {
+    return gulp.src(['assets/js/nunchuk/*js'])
+        .pipe(concat('nunchuk.js'))
         .pipe(gulp.dest('.tmp/public/javascripts'))
 });
 
-
-gulp.task('copylivereload', function() {
-    return gulp.src('assets/js/*.js')
-        .pipe(concat('livereload.js'))
-        .pipe(gulp.dest('.tmp/public/javascripts'))
-});
-
-// Views
-gulp.task('views', function() {
+gulp.task('copyhtml', function() {
     return gulp.src('views/*.html')
         .pipe(gulp.dest('.tmp/public/'))
 });
 
-// Watch
-gulp.task('watch', function() {
-    // run these tasks on watch start.
-    gulp.start(['copylivereload','styles', 'scripts', 'views'])
-    livereload()
-    livereload.listen();
-    // run these tasks when files change.
-    gulp.watch(['views/*.html'], ['views']);
-    gulp.watch(['assets/less/*.less'], ['styles']);
-    gulp.watch(['assets/js/*.js'], ['scripts']);
-    // reload these files
-    gulp.watch(['views/*.html']).on('change', livereload.changed);
-    gulp.watch(['.tmp/public/css/*.css']).on('change', livereload.changed);
-    gulp.watch(['.tmp/public/javascripts/*.js']).on('change', livereload.changed);
+gulp.task('copylivereload', function() {
+    return gulp.src('assets/js/livereload/livereload.js')
+        .pipe(gulp.dest('.tmp/public/javascripts'))
+});
 
+gulp.task('build', ['clean'], function() {
+    gulp.start('compless', 'concatjs', 'copyhtml', 'copylivereload');
+});
+
+gulp.task('watch', function() {
+
+    //start live reload
+    livereload();
+    //listen default port.
+    livereload.listen();
+
+    // run build task when any of these files change.
+    gulp.watch(['views/*.html'], ['copyhtml']);
+    gulp.watch(['assets/javascripts/*.js'], ['concatjs']);
+    gulp.watch(['assets/jsx/*.jsx'], ['concatjs']);
+    gulp.watch(['assets/less/*.less'], ['compless'])
+        // reload these files
+    gulp.watch(['.tmp/public/*.html']).on('change', livereload.changed);
+    gulp.watch(['.tmp/public/css/*.css']).on('change', livereload.changed);
+    gulp.watch(['.tmp/public/javascripts/nunchuk.js']).on('change', livereload.changed);
 });
